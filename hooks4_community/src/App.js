8355ab834c24bcaf1ac2558e4efa99eef2fa7415
rudtjd2548 <a92667441@gmail.com>
2019-02-20 14:47:31 +1300

    let's make community website!

---
+++ hooks4_community/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>Hello</h1>
+    </React.Fragment>
+  );
+};
+
+export default App;
