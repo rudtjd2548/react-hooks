61a64c758b1e6476cf14712ac0e9befe021feb14
rudtjd2548 <a92667441@gmail.com>
2019-02-25 23:20:07 +1300

    google auth added

--- hooks5_udemy/client/src/components/Auth/Login.js
+++ hooks5_udemy/client/src/components/Auth/Login.js
@@ -1,18 +1,46 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { GraphQLClient } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from '@material-ui/core/styles';
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `
+  {
+    me{
+      _id
+      name
+      email
+      picture
+    }
+  }
+`;
+
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = async googleUser => {
+    const idToken = googleUser.getAuthResponse().id_token;
+    const client = new GraphQLClient('http://localhost:4000/graphql', {
+      headers: { authorization: idToken }
+    });
+    const data = await client.request(ME_QUERY);
+    console.log({ data });
+  };
+
+  return (
+    <GoogleLogin
+      clientId='203657272501-il760t029jccqvgfv3ije60d1in15tbm.apps.googleusercontent.com'
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />
+  );
 };
 
 const styles = {
   root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
   }
 };
 
