91bb48893c29cda3600f7e3750cfad3a0d597e29
rudtjd2548 <a92667441@gmail.com>
2019-02-25 19:19:27 +1300

    new udemy lecture setup with mongoose

---
+++ hooks5_udemy/resolvers.js
@@ -0,0 +1,12 @@
+const user = {
+  _id: '1',
+  name: 'Reed',
+  email: 'reedbarger@yahoo.com',
+  picture: 'https://cloudinary.com/asdf'
+};
+
+module.exports = {
+  Query: {
+    me: () => user
+  }
+};
